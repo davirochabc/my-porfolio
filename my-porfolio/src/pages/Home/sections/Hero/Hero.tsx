92a6866a5becc2d450b1524ce92269e760a815cc
Davi Rocha <100158727+davirochabc@users.noreply.github.com>
2025-09-16 15:30:03 +0000

    save state01

--- my-porfolio/src/pages/Home/sections/Hero/Hero.tsx
+++ my-porfolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,20 +1,37 @@
-import { styled } from "@mui/material"
-
-
+import { Grid, styled } from "@mui/material"
+import Avatar from "../../../../assets/images/images.jpg"
 const Hero = () => {
- 
-    const StyledHero = styled("div")(()=> ({
-        backgroundColor: "black"
-    }))
-        
-    
+
+  const StyledHero = styled("div")(() => ({
+    backgroundColor: "black"
+  }))
+
+  const StyledImg = styled("img")(() => ({
+    borderRadius: "50%",
+  }))
+
 
   return (
     <>
-        <StyledHero>
-            timbalada
-        </StyledHero>
-     
+      <StyledHero>
+        <Grid container spacing={2}>
+          <Grid size={8}>
+            size=8
+         </Grid>
+          <Grid size={4}>
+            size=4
+         </Grid>
+          <Grid size={4}>
+            size=4
+         </Grid>
+          <Grid size={8}>
+            size=8
+         </Grid>
+        </Grid>
+
+        <StyledImg src={Avatar} alt="Avatar" />
+      </StyledHero>
+
     </>
   )
 }
